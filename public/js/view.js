fc4594f190f0a00087b1146f223103de1842a629
Benson Che <coffeeaddictsunited@gmail.com>
2015-03-05 01:05:31 -0900

    Add template for each cell

--- public/js/view.js
+++ public/js/view.js
@@ -10,6 +10,18 @@
 
 		render: function() {
 			var cells = this.$el.find(".cell");
+
+			var template = $("#mainCellTemplate").html();
+
+			cells.each(function(index, value) {
+				var compiled = _.template(template);
+
+				var $value = $(value);
+
+				var name = $value.attr("data-name");
+
+				$(value).html(compiled({name: name}));
+			});
 		}
 	});
 })(dq);
